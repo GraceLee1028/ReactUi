036d8b5eecd1e2791d5c8be53d5055b022d0b6b3
lifeng <15823904221@163.com>
2023-01-13 08:43:11 +0800

    react启动项目

--- my-todolist/src/index.js
+++ my-todolist/src/index.js
@@ -6,15 +6,30 @@ class Todo extends React.Component{
   }
 }
 class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      list:['打工','运动','阅读','旅行']
+    }
+  }
+  componentDidMount(){
+    this.setState({
+      list:[...this.state.list,'发呆']
+    })
+  }
   render(){
-    const list = ['打工','运动','阅读','旅行'];
     return <div>
-      <Todo text={list[0]}></Todo>
-      <Todo text={list[1]}></Todo>
-      <Todo text={list[2]}></Todo>
-      <Todo text={list[3]}></Todo>
+      <Todo text={this.state.list[0]}></Todo>
+      <Todo text={this.state.list[1]}></Todo>
+      <Todo text={this.state.list[2]}></Todo>
+      <Todo text={this.state.list[3]}></Todo>
+      <Todo text={this.state.list[4]}></Todo>
     </div>
   }
 }
-
+//17之前的api
 ReactDOM.render(<App />,document.getElementById('root'))
+//18之后新的api
+// const rootEle = document.getElementById('root')
+// const root = ReactDOM.createRoot(rootEle)
+// root.render(<App cal)
