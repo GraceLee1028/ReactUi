205bfbf88292d4fc9bb987f5f83f69847b485d80
lifeng <15823904221@163.com>
2023-06-09 17:24:55 +0800

    调整

---
+++ learnreact/src/components/galley.js
@@ -0,0 +1,11 @@
+import Profile from "./Profile";
+
+export default function Gallery(){
+  return (
+    <div class="Gallery">
+      <Profile/>
+      <Profile/>
+      <Profile/>
+    </div>
+  )
+}
