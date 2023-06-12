51beeb2fb38006555154f5baff953bb03bed8018
lifeng <15823904221@163.com>
2023-06-12 19:15:52 +0800

    测试

--- learnreact/src/components/Profile/index.js
+++ learnreact/src/components/Profile/index.js
@@ -1,8 +1,11 @@
-function Profile(){
+function Profile({name}){
   return (
+    <>
+    {name.imageId}{name.imageSize}
     <img src="https://i.imgur.com/MK3eW3Am.jpg"
       alt="Katherine Johnson"
     />
+    </>
   )
 }
 export default Profile
--- learnreact/src/components/galley.js
+++ learnreact/src/components/galley.js
@@ -1,9 +1,13 @@
 import Profile from "./Profile";
 
 export default function Gallery(){
+  const person = {
+    imageId:'123',
+    imageSize:12
+  }
   return (
-    <div class="Gallery">
-      <Profile/>
+    <div className="gallery">
+      <Profile name={person.imageId+person.imageSize+'.jpg'}/>
       <Profile/>
       <Profile/>
     </div>
