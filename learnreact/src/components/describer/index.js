804afcfd6cb6c0dc6dcef28a5cd50b6639afcfe8
lifeng <15823904221@163.com>
2023-08-15 14:05:19 +0800

    调整

--- learnreact/src/components/describer/index.js
+++ learnreact/src/components/describer/index.js
@@ -5,15 +5,17 @@ class Describer extends React.Component{
   constructor(props){
     super(props)
     this.state = {status:0}
+    this.toggleTwo = this.toggle.bind(this)
   }
   render(){
     return <>
       <p>今天的天气：{this.state.status?'炎热':'凉爽'}</p>
-      <button onClick={()=>this.toggle()}>切换天气</button>
+      <button onClick={this.toggleTwo}>切换天气</button>
     </>
   }
   toggle(){
-    this.setState({status:this.state.status===0?1:0})
+    console.log(this)
+    // this.setState({status:this.state.status===0?1:0})
   }
 }
 export default Describer
