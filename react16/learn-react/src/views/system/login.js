f99fd3606aae3d6f8fc2ea5e2f21308a3c986348
lifeng <15823904221@163.com>
2024-02-28 10:24:03 +0800

    componentDidMount获取数据

--- react16/learn-react/src/views/system/login.js
+++ react16/learn-react/src/views/system/login.js
@@ -1,15 +1,17 @@
 import React from "react";
 import List from './list'
+import axios from "axios"
 class Login extends React.Component{
     constructor(props){
         super(props)
         this.state = {
             name:'',
-            pwd:""
+            pwd:"",
+            history:[]
         }
         console.log('constructor挂载前===========')
     }
-   // 用 render 方法之前调用
+    // 用 render 方法之前调用
     //getDerivedStateFromProps 的存在只有一个目的：让组件在 props 变化时更新 state
     static getDerivedStateFromProps(props){
         console.log("getDrivedStateFromProps============",props)
@@ -28,7 +30,19 @@ class Login extends React.Component{
     //     localStorage.setItem('test','1')
     //     console.log('将要挂载时调用=========componentWillMount')
     // }
+    //常用于请求数据
     componentDidMount(){
+        axios.get("/json/user.json").then(res=>{
+            const d =res.data;
+            console.log('current userinfo',d)
+            this.setState({
+                name:d.name,
+                pwd:d.pwd,
+                history:[...this.state.history,
+                    {name:d.name,
+                    pwd:d.pwd}]
+            })
+        })
         console.log('挂载完后调用=========componentDidMount')
     }
     componentWillUnmount(){
@@ -39,7 +53,12 @@ class Login extends React.Component{
     pwdRef = null//ref的回调用法
     render(){
         console.log("render=========login")
+        const userHistory = this.state.history.map(item=>{
+            return <p key={item.name}>用户名：{item.name},密码：{item.pwd}</p>
+        })
         return <>
+        <h1>历史登录信息</h1>
+        {userHistory}
         <List />
         <label >用户名：<input  ref={this.nameRef}  type="text" /></label>
         <label >密码：<input ref={(ref)=>{this.pwdRef=ref}} type="password" /></label>
@@ -47,11 +66,17 @@ class Login extends React.Component{
         <p>当前登录信息为:<br/>用户名：{this.state.name} 密码：{this.state.pwd}</p>
         </>
     }
+    getUserInfo(){
+
+    }
     loginDeal=()=>{
         console.log(this.nameRef)
+        const o = {pwd:this.pwdRef.value,
+        name:this.nameRef.current.value}
         this.setState({
-            pwd:this.pwdRef.value,
-            name:this.nameRef.current.value
+            pwd:o.pwd,
+            name:o.name,
+            history:[o,...this.state.history]
         })
     }
 }
