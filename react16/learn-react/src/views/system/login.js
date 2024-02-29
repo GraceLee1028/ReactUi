fc60e414212fc2d73b080148be4c07e9685faa1a
lifeng <15823904221@163.com>
2024-02-29 09:46:54 +0800

    添加配置

--- react16/learn-react/src/views/system/login.js
+++ react16/learn-react/src/views/system/login.js
@@ -14,12 +14,12 @@ class Login extends React.Component{
     // 用 render 方法之前调用
     //getDerivedStateFromProps 的存在只有一个目的：让组件在 props 变化时更新 state
     static getDerivedStateFromProps(props){
-        console.log("getDrivedStateFromProps============",props)
+        console.log("getDrivedStateFromProps============",props);
         return null
     }
     //在最近一次渲染输出之前调用
     getSnapshotBeforeUpdate(d){
-        console.log("getSnapshotBeforeUpdate=============更新之前获取snapshot")
+        console.log("getSnapshotBeforeUpdate=============更新之前获取snapshot");
         console.log(d)
         return null
     }
@@ -32,17 +32,7 @@ class Login extends React.Component{
     // }
     //常用于请求数据
     componentDidMount(){
-        axios.get("/json/user.json").then(res=>{
-            const d =res.data;
-            console.log('current userinfo',d)
-            this.setState({
-                name:d.name,
-                pwd:d.pwd,
-                history:[...this.state.history,
-                    {name:d.name,
-                    pwd:d.pwd}]
-            })
-        })
+        this.getUserInfo()
         console.log('挂载完后调用=========componentDidMount')
     }
     componentWillUnmount(){
@@ -52,7 +42,7 @@ class Login extends React.Component{
     nameRef=React.createRef()// createRef的用法【最推荐用法】
     pwdRef = null//ref的回调用法
     render(){
-        console.log("render=========login")
+        // console.log("render=========login")
         const userHistory = this.state.history.map(item=>{
             return <p key={item.name}>用户名：{item.name},密码：{item.pwd}</p>
         })
@@ -67,7 +57,17 @@ class Login extends React.Component{
         </>
     }
     getUserInfo(){
-
+        axios.get("/json/user.json").then(res=>{
+            const d =res.data;
+            console.log('current userinfo',d)
+            this.setState({
+                name:d.name,
+                pwd:d.pwd,
+                history:[...this.state.history,
+                    {name:d.name,
+                    pwd:d.pwd}]
+            })
+        })
     }
     loginDeal=()=>{
         console.log(this.nameRef)
